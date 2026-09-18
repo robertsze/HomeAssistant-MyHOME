b699609b97511ee883d1f857bc5cb7a072664042
robertsze <szeleneyr@gmail.com>
2026-09-18 11:10:15 +0200

    Fix

--- custom_components/myhome/validate.py
+++ custom_components/myhome/validate.py
@@ -451,13 +451,6 @@ media_player_schema = MyHomeDeviceSchema(
 gateway_schema = Schema(
     {
         Required(CONF_MAC): MacAddress(),
-        Optional(LIGHT): light_schema,
-        Optional(SWITCH): switch_schema,
-        Optional(COVER): cover_schema,
-        Optional(BINARY_SENSOR): binary_sensor_schema,
-        Optional(SENSOR): sensor_schema,
-        Optional(CLIMATE): climate_schema,
-        Optional(MEDIA_PLAYER): media_player_schema,
         Optional(LIGHT): lambda v: light_schema(v),
         Optional(SWITCH): lambda v: switch_schema(v),
         Optional(COVER): lambda v: cover_schema(v),
@@ -467,19 +460,4 @@ gateway_schema = Schema(
         Optional(MEDIA_PLAYER): lambda v: media_player_schema(v),
     }
 )
-
-
-gateway_schema = Schema(
-    {
-        Required(CONF_MAC): MacAddress(),
-        Optional(LIGHT): light_schema,
-        Optional(SWITCH): switch_schema,
-        Optional(COVER): cover_schema,
-        Optional(BINARY_SENSOR): binary_sensor_schema,
-        Optional(SENSOR): sensor_schema,
-        Optional(CLIMATE): climate_schema,
-        Optional(MEDIA_PLAYER): media_player_schema,
-    }
-)
-
 config_schema = MyHomeConfigSchema({Required(str): gateway_schema})
